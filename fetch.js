a4f5248fb34877199c899eacef1d066b3568e4fa
ruioliva <31671195+ruioliva@users.noreply.github.com>
2021-02-19 00:11:42 +0000

    "added fetch.js"

---
+++ fetch.js
@@ -0,0 +1,11 @@
+// Fetch
+
+fetch('todos/luigi.json').then((response) => {
+    console.log('resolved', response);
+    return response.json();
+}).then(data => {
+    console.log(data);
+})
+.catch ((err) => {
+    console.log('rejected', err)
+});
